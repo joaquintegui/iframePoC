d8220752a4054ecfc9349e73136cf6fcd532feae
JotaTegui <joaquin.inurrategui@gmail.com>
2024-07-02 19:46:22 -0300

    largemodalinput

---
+++ largeModal.js
@@ -0,0 +1,8 @@
+document.getElementById('sendMessageInput').addEventListener('change', (event) => {
+    const message = {
+    topic: 'modalInput',
+    value: event.target.value
+}
+});
+
+window.parent.postMessage(message, '*');
