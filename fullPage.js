f660e05b71d0ca47614151b2d3a06cad65b6145b
JotaTegui <joaquin.inurrategui@gmail.com>
2024-07-03 18:13:40 -0300

    modal

--- fullPage.js
+++ fullPage.js
@@ -35,7 +35,27 @@ document.getElementById('openModalInIframe').addEventListener('click', function(
     console.log('modalopen');
     document.getElementById('modal').classList.add('open');
     document.body.classList.add('jw-modal-open');
+    
+    const message = {
+        topic: 'openInternalModal',
+        oppId: oppId,
+    };
+
+    window.parent.postMessage(message, '*');
 });
+
+document.getElementById('closeModal').addEventListener('click', function() {
+    document.querySelector('.jw-modal.open').classList.remove('open');
+    document.body.classList.remove('jw-modal-open');
+
+    const message = {
+        topic: 'closeInternalModal',
+        oppId: oppId,
+    };
+
+    window.parent.postMessage(message, '*');
+});
+
 
 var sleep = duration => new Promise(resolve => setTimeout(resolve, duration))
 var poll = (promiseFn, duration) => promiseFn().then(
